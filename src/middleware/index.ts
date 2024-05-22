a785c4052f3d7385d1ef899fff2c1d7a0666802d
Maddy <maddy@Maddys-MacBook-Pro.local>
2024-05-22 15:16:04 +0500

    JWT Fixed

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,26 +1,62 @@
-import {Response, Request, NextFunction} from "express";
-import jwt, {JwtPayload} from "jsonwebtoken";
+import { Response, Request, NextFunction } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import User from "../models/user_model";
 
-
 export interface AuthRequest extends Request {
-    user: string
+  user: string;
 }
 
-export const authenticationMiddleware = async (request: AuthRequest, response: Response, next: NextFunction) => {
-    try {
-    const {authorization} = request.headers;
-        if(!authorization){
-        return response.status(401).json({error: 'Authorization failed Please login again'})
-        }
+// export const authenticationMiddleware = async (
+//   request: AuthRequest,
+//   response: Response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     const { authorization } = request.headers;
+//     if (!authorization) {
+//       return response
+//         .status(401)
+//         .json({ error: "Authorization failed Please login" });
+//     }
 
-        const {_id} = jwt.verify(authorization, 'token') as JwtPayload;
-        const existingUser = await User.findOne(_id)
-        if(existingUser){
-            request.user = existingUser.id
-        }
-        next()
-    }catch (error){
-        return response.status(500).send({message: 'Oops! Something went wrong', error})
+//     const { _id } = jwt.verify(authorization, "token") as JwtPayload;
+//     const existingUser = await User.findOne(_id);
+//     if (existingUser) {
+//       request.user = existingUser.id;
+//     }
+//     next();
+//   } catch (error) {
+//     return response
+//       .status(500)
+//       .send({ message: "Oops! Something went wrong", error });
+//   }
+// };
+
+export const authenticationMiddleware = async (
+  request: AuthRequest,
+  response: Response,
+  next: NextFunction
+) => {
+  try {
+    const { authorization } = request.headers;
+    if (!authorization) {
+      return response
+        .status(401)
+        .json({ error: "Authorization failed. Please login." });
     }
-}
+
+    const token = authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.USER_TOKEN) as JwtPayload;
+    const existingUser = await User.findById(decoded.id);
+    if (!existingUser) {
+      return response.status(401).json({ error: "User not found." });
+    }
+
+    request.user = existingUser._id;
+    next();
+  } catch (error) {
+    return response
+      .status(500)
+      .send({ message: "Oops! Something went wrong", error });
+  }
+};
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,38 +1,39 @@
 export interface IUser {
-    name: string,
-    email: string,
-    password: string
+  _id: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
 export interface IColor {
-    name: string,
-    id: string,
-    code: string
+  name: string;
+  id: string;
+  code: string;
 }
 
 export interface IIcon {
-    name: string,
-    id: string,
-    symbol: string
+  name: string;
+  id: string;
+  symbol: string;
 }
 
 export interface ICategory {
-    _id: string,
-    name: string,
-    user: IUser | string,
-    isEditable: boolean,
-    color: IColor
-    icon: IIcon
+  _id: string;
+  name: string;
+  user: IUser | string;
+  isEditable: boolean;
+  color: IColor;
+  icon: IIcon;
 }
 
 export interface ITasks {
-    _id: string,
-    name: string,
-    categoryId: ICategory | string,
-    user: string,
-    isCompleted: boolean,
-    isEditable: boolean,
-    date: string
-    createdAt: string,
-    updatedAt: string
+  _id: string;
+  name: string;
+  categoryId: ICategory | string;
+  user: string;
+  isCompleted: boolean;
+  isEditable: boolean;
+  date: string;
+  createdAt: string;
+  updatedAt: string;
 }
